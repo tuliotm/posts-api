274e530f704fa1c38472d45210308f4fbb974129
tuliotm <tulioteodoromanso@gmail.com>
2024-05-06 15:30:17 -0300

    Spec class methods

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,4 +13,65 @@ RSpec.describe(Post, type: :model) do
     it { should belong_to(:user) }
     it { should have_many(:ratings) }
   end
+
+  describe ".user_post" do
+    context "when user is found or created" do
+      let(:params) { { user_login: "testuser", title: "Test Post", body: "This is a test post", ip: "127.0.0.1" } }
+
+      it "creates a new post associated with the user" do
+        expect do
+          Post.user_post(params)
+        end.to(change(Post, :count).by(1))
+
+        post = Post.last
+        expect(post.title).to(eq("Test Post"))
+        expect(post.body).to(eq("This is a test post"))
+        expect(post.ip).to(eq("127.0.0.1"))
+        expect(post.user.login).to(eq("testuser"))
+      end
+    end
+
+    context "when user is not persisted" do
+      let(:params) do
+        { title: "Test Post", body: "This is a test post" }
+      end
+
+      it "does not create a new post" do
+        expect do
+          Post.user_post(params)
+        end.not_to(change(Post, :count))
+
+        expect(Post.user_post(params)).to(be_nil)
+      end
+    end
+  end
+
+  describe ".top_rated" do
+    let!(:post1) { create(:post) }
+    let!(:post2) { create(:post) }
+    let!(:rating1) { create(:rating, post: post1, value: 5) }
+    let!(:rating2) { create(:rating, post: post1, value: 4) }
+    let!(:rating3) { create(:rating, post: post2, value: 3) }
+
+    it "returns the posts with the highest average ratings" do
+      top_rated = Post.top_rated(1)
+      expect(top_rated.length).to(eq(1))
+      expect(top_rated.first).to(eq(post1))
+    end
+  end
+
+  describe ".authors_ips" do
+    let!(:user1) { create(:user, login: "user1") }
+    let!(:user2) { create(:user, login: "user2") }
+    let!(:post1) { create(:post, user: user1, ip: "127.0.0.1") }
+    let!(:post2) { create(:post, user: user2, ip: "127.0.0.1") }
+    let!(:post3) { create(:post, user: user1, ip: "192.168.0.1") }
+
+    it "returns the IPs that are used by multiple authors" do
+      authors_ips = Post.authors_ips
+      expect(authors_ips.length).to(eq(1))
+      expect(authors_ips.first[:ip]).to(eq("127.0.0.1"))
+      expect(authors_ips.first[:authors]).to(contain_exactly("user1", "user2"))
+    end
+  end
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -15,4 +15,64 @@ RSpec.describe(Rating, type: :model) do
     it { should belong_to(:post) }
     it { should belong_to(:user) }
   end
+
+  describe ".create_and_calculate_average" do
+    let(:post) { create(:post) }
+    let(:params) do
+      {
+        user_id: post.user.id,
+        post_id: post.id,
+        value: 5,
+      }
+    end
+
+    context "when the post is not found" do
+      it "returns status :not_found and an error message" do
+        params[:post_id] = -1
+        result = Rating.create_and_calculate_average(params)
+        expect(result[:status]).to(eq(:not_found))
+        expect(result[:errors]).to(include("Post not found."))
+      end
+    end
+
+    context "when the post has already been rated by the user" do
+      before do
+        create(:rating, user: post.user, post: post)
+      end
+
+      it "returns status :unprocessable_entity and an error message" do
+        result = Rating.create_and_calculate_average(params)
+        expect(result[:status]).to(eq(:unprocessable_entity))
+        expect(result[:errors]).to(include("Post already rated."))
+      end
+    end
+
+    context "when the rating is successfully created" do
+      it "creates a new rating" do
+        expect { Rating.create_and_calculate_average(params) }.to(change { Rating.count }.by(1))
+      end
+
+      it "returns status :created and the average rating" do
+        result = Rating.create_and_calculate_average(params)
+        expect(result[:status]).to(eq(:created))
+        expect(result[:average]).to(eq(5.0))
+      end
+    end
+
+    context "when the rating creation fails" do
+      before do
+        params[:value] = 6
+      end
+
+      it "does not create a new rating" do
+        expect { Rating.create_and_calculate_average(params) }.not_to(change { Rating.count })
+      end
+
+      it "returns status :unprocessable_entity and error messages" do
+        result = Rating.create_and_calculate_average(params)
+        expect(result[:status]).to(eq(:unprocessable_entity))
+        expect(result[:errors]).to(include("Value is not included in the list"))
+      end
+    end
+  end
 end
