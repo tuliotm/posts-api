7c072ad5810cfce2e9a4195189a7e31023efa56f
tuliotm <tulioteodoromanso@gmail.com>
2024-06-10 19:30:11 -0300

    Removed controller logic from model

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,21 +18,4 @@ class Post < ApplicationRecord
       .order("average_rating DESC")
       .limit(limit)
   }
-
-  #== CLASS METHODS ======================================
-  class << self
-    def authors_ips
-      ips = select(:ip).distinct
-      result = []
-
-      ips.each do |ip_record|
-        users = User.joins(:posts).where(posts: { ip: ip_record.ip }).distinct
-        if users.count > 1
-          result << { ip: ip_record.ip, authors: users.pluck(:login) }
-        end
-      end
-
-      result
-    end
-  end
 end
